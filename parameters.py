6570c0b91f06984cefb01778d4386d2c4cd6bf20
Alfie <alfielenton@gmail.com>
2025-12-28 17:52:41 +0000

    Changed C parameter

--- parameters.py
+++ parameters.py
@@ -3,8 +3,8 @@ class Parameters:
 
     def __init__(self,irrelevant):
 
-        self.C = 10
-        self.sig_y = 2.5
+        self.C = 100
+        self.sig_y = 1.
         self.num_components = 1500
         self.sparse_proportion = 3/4
         self.x_dim = 4
